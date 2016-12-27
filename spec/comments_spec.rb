92db6f264945981c3dd40e2eb4661ef0267c555e
Titus Fortner <titusfortner@gmail.com>
2016-12-27 00:15:09 -0600

    get specs working

--- spec/comments_spec.rb
+++ spec/comments_spec.rb
@@ -1,4 +1,4 @@
-require_relative "spec_helper"
+require "spec_helper"
 
 describe "Guinea Pig Comments" do
   it "Verify comments text" do
--- spec/link_spec.rb
+++ spec/link_spec.rb
@@ -1,4 +1,4 @@
-require_relative "spec_helper"
+require "spec_helper"
 
 describe "Guinea Pig Link" do
   it "Verify Link opens page with correct title" do
