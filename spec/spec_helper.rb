5a4a11f56482ea73ea9e070f1ccaa0b6008036e2
Titus Fortner <titusfortner@gmail.com>
2019-12-06 00:05:06 -0500

    implemented with simple sauce 0.1

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,6 @@ require 'simple_sauce'
 RSpec.configure do |config|
   config.before(:each) do |example|
     opts = platform.merge(name: example.full_description,
-                          screen_resolution: '1080x720',
                           command_timeout: 90)
     sauce_options = SimpleSauce::Options.new(opts)
 
@@ -33,10 +32,10 @@ RSpec.configure do |config|
     case ENV['PLATFORM']
     when 'windows_10_edge'
       {platform_name: 'Windows 10',
-       browser_name: 'edge'}
+       browser_name: 'MicrosoftEdge'}
     when 'windows_8_ie'
-      {platform: 'Windows 8.1',
-       browser_name: 'ie'}
+      {platform_name: 'Windows 8.1',
+       browser_name: 'internet explorer'}
     when 'mac_sierra_chrome'
       {platform_name: 'macOS 10.12',
        browser_name: 'chrome'}
